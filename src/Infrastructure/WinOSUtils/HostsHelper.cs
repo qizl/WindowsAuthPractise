5b5c33df478a72ff0c231d24bf0e9810c8415ecd
qizl <qizl@glodon.com>
2022-04-01 18:10:56 +0800

    调整项目结构

---
+++ src/Infrastructure/WinOSUtils/HostsHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace WinOSUtils
+{
+    public class HostsHelper
+    {
+        public static void DisabledChromeUpdate()
+        {
+            /*
+             * 添加以下配置，禁止chrome更新：
+             *  127.0.0.1   clients2.google.com
+             *  127.0.0.1   update.googleapis.com
+             *  127.0.0.1   redirector.gvt1.com
+             */
+
+        }
+    }
+}
